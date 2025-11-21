83d049326eb78ea5fbddf7ea54a9778efe2b2660
Nick-cms <Nikto159357@mail.ru>
2025-11-21 19:38:23 +0000

    Лабораторная работа №7

--- lab7/src/tcpserver.c
+++ lab7/src/tcpserver.c
@@ -2,21 +2,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
 #include <sys/socket.h>
 #include <sys/types.h>
 #include <unistd.h>
 
-#define SERV_PORT 10050
-#define BUFSIZE 100
 #define SADDR struct sockaddr
 
-int main() {
+int main(int argc, char *argv[]) {
+  if (argc < 3) {
+    printf("Usage: %s <PORT> <BUFSIZE>\n", argv[0]);
+    exit(1);
+  }
+
+  int port = atoi(argv[1]);
+  int bufsize = atoi(argv[2]);
   const size_t kSize = sizeof(struct sockaddr_in);
 
   int lfd, cfd;
   int nread;
-  char buf[BUFSIZE];
+  char buf[bufsize];
   struct sockaddr_in servaddr;
   struct sockaddr_in cliaddr;
 
@@ -28,7 +32,7 @@ int main() {
   memset(&servaddr, 0, kSize);
   servaddr.sin_family = AF_INET;
   servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
-  servaddr.sin_port = htons(SERV_PORT);
+  servaddr.sin_port = htons(port);
 
   if (bind(lfd, (SADDR *)&servaddr, kSize) < 0) {
     perror("bind");
@@ -49,7 +53,7 @@ int main() {
     }
     printf("connection established\n");
 
-    while ((nread = read(cfd, buf, BUFSIZE)) > 0) {
+    while ((nread = read(cfd, buf, bufsize)) > 0) {
       write(1, &buf, nread);
     }
 
